deb487bbdaa1f89c7bee05dabca4136d0c17f303
Guilherme Ferrari <guiferrarib@hotmail.com>
2025-03-06 19:49:22 -0300

    Remove unused imports

--- app/src/main/java/com/example/interviewtest/presentation/view/LoginScreen.kt
+++ app/src/main/java/com/example/interviewtest/presentation/view/LoginScreen.kt
@@ -1,18 +1,13 @@
 package com.example.interviewtest.presentation.view
 
 import android.app.Application
-import android.widget.EditText
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.input.rememberTextFieldState
-import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,7 +15,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
