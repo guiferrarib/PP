288277c966406ffc38c35a96dda62fe63d7614b3
Guilherme Ferrari <guiferrarib@hotmail.com>
2025-03-06 19:48:25 -0300

    Clear LoginUiState class

--- app/src/main/java/com/example/interviewtest/presentation/model/LoginUiState.kt
+++ app/src/main/java/com/example/interviewtest/presentation/model/LoginUiState.kt
@@ -2,9 +2,6 @@ package com.example.interviewtest.presentation.model
 
 data class LoginUiState(
     val userLogin: UserLogin? = null,
-    val isLoading: Boolean? = null,
+    val isLoading: Boolean = false,
     val error: String? = null,
-    val isLoginSuccess: Boolean? = null,
-    val isLoginFailed: Boolean? = null,
-    val isLoginButtonEnabled: Boolean? = null,
 )
